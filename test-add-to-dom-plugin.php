f1b0b0e068747ef0c679de119d07822434f2c1fd
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-01-22 16:23:26 -0700

    test trying to change the product border image green

--- test-add-to-dom-plugin.php
+++ test-add-to-dom-plugin.php
@@ -1,59 +1,65 @@
 <?php
 /*
- * Plugin Name:     Test Add to Dom Plugin
- * Plugin URI:      https://github.com/DreamTeamSeven/test-add-to-dom-plugin
- * Description:     A plugin to test adding to the DOM
- * Version:         0.0.1
+ * Plugin Name:       Test Add to Dom Plugin
+ * Plugin URI:        https://github.com/DreamTeamSeven/test-add-to-dom-plugin
+ * Description:       A plugin to test adding to the DOM
+ * Version:           0.0.0
  * Requires at least: 6.7.1
- * Requires PHP:    8.3
- * Author:          Dream Team Seven
- * Author URI:      https://github.com/DreamTeamSeven
- * License:         No License
- * License URI:     https://choosealicense.com/no-permission
- * Update URI:      https://github.com/DreamTeamSeven/test-add-to-dom-plugin
- * Text Domain:     test-add-to-dom-plugin
- * Requires Plugins: WooCommerce
+ * Requires PHP:      8.3
+ * Author:            Dream Team Seven
+ * Author URI:        https://github.com/DreamTeamSeven
+ * License:           No License
+ * License URI:       https://choosealicense.com/no-permission
+ * Update URI:        https://github.com/DreamTeamSeven/test-add-to-dom-plugin
+ * Text Domain:       test-add-to-dom-plugin
+ * Requires Plugins:  WooCommerce
  */
 
-function test_add_to_dom_plugin() {
-    if ( !is_plugin_active( 'woocommerce/woocommerce.php' ) ) {
-        add_action( 'admin_notices', 'test_add_to_dom_plugin_notice' );
-        return;
+
+function test_add_to_dom_plugin()
+{
+    // Test to see if WooCommerce is active (including network activated).
+    $plugin_path = trailingslashit(WP_PLUGIN_DIR) . 'woocommerce/woocommerce.php';
+
+    if (
+        in_array($plugin_path, wp_get_active_and_valid_plugins())
+        || in_array($plugin_path, wp_get_active_network_plugins())
+    ) {
+        // Custom code here. WooCommerce is active, however it has not 
+        // necessarily initialized (when that is important, consider
+        // using the `woocommerce_init` action).
     }
 }
 
-function test_add_to_dom_plugin_notice() {
-    ?>
-    <div class="notice notice-warning is-dismissible">
-        <p><?php _e( 'Test Add to Dom Plugin requires WooCommerce to be installed and activated.', 'test-add-to-dom-plugin' ); ?></p>
-    </div>
-    <?php
-}
-
-function my_custom_header_element() {
+function my_custom_header_element()
+{
+    // Your custom header element code here
     echo '<p style="background-color: #f0f0f0; padding: 10px;">This is my custom header element!</p>';
 }
 
+// Hook to add circle buttons
 function add_circle_buttons() {
-    if ( is_product() ) {
+    global $product;
+
+    // Check if we're on a product page
+    if (is_product()) {
         ?>
         <div class="circle-buttons">
             <button class="circle-button green-button" id="green-border-button">Button 1</button>
-            <button class="circle-button red-button" id="red-border-button">Button 2</button>
         </div>
         <?php
     }
 }
 
-function enqueue_scripts() {
-    wp_enqueue_style( 'circle-button-css', plugins_url( 'circle-button.css', __FILE__ ) );
-    wp_enqueue_script( 'circle-button-js', plugins_url( 'circle-button.js', __FILE__ ), array( 'jquery' ), '1.0', true ); // Added jQuery dependency
+// Hook to enqueue circle button CSS
+function enqueue_circle_button_css() {
+    wp_enqueue_style('circle-button-css', plugins_url('circle-button.css', __FILE__));
 }
 
 test_add_to_dom_plugin();
 
-add_action( 'wp_body_open', 'my_custom_header_element' );
-add_action( 'woocommerce_before_add_to_cart_button', 'add_circle_buttons' );
-add_action( 'wp_enqueue_scripts', 'enqueue_scripts' );
+// Add actions
+add_action('woocommerce_before_add_to_cart_button', 'add_circle_buttons');
+add_action('wp_enqueue_scripts', 'enqueue_circle_button_css');
 
-?>
+
