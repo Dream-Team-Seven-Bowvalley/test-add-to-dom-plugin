30b3d2e152c7614ff5442768e3f25b738a3348cb
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-01-22 16:24:37 -0700

    test

--- circle-button.js
+++ circle-button.js
@@ -1,9 +1,5 @@
 jQuery(document).ready(function($) {
     $('#green-border-button').click(function() {
-        $('.woocommerce-product-gallery').removeClass('red-border').addClass('green-border');
-    });
-
-    $('#red-border-button').click(function() {
-        $('.woocommerce-product-gallery').removeClass('green-border').addClass('red-border');
+        $('.woocommerce-product-gallery__image').addClass('green-border');
     });
 });
