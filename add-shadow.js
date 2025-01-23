e07a2f912281b286ef080f565d1f995fae5d5a87
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-01-22 17:03:33 -0700

    update js

--- add-shadow.js
+++ add-shadow.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.querySelector('.circle-button.green-button');
-    if (button) {
+    const productImage = document.querySelector('.woocommerce div.product div.images img');
+
+    if (button && productImage) {
         button.addEventListener('click', function(event) {
             event.preventDefault();
-            // Add your custom button click logic here
+            productImage.style.boxShadow = '0 0 10px 5px green';
         });
     }
 });
