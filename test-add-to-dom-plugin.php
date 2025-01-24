9efc79e887dd661e32c2198d52f5be76b665d161
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-01-24 16:28:23 -0700

    test

--- test-add-to-dom-plugin.php
+++ test-add-to-dom-plugin.php
@@ -95,7 +95,7 @@ function add_green_shadow_to_product_image()
 test_add_to_dom_plugin();
 
 // Add actions
-add_action('woocommerce_before_add_to_cart_button', 'add_circle_bttns');
+// add_action('woocommerce_before_add_to_cart_button', 'add_circle_bttns');
 add_action('woocommerce_before_add_to_cart_button', 'add_custom_button_above_add_to_cart');
 add_action('wp_enqueue_scripts', 'enqueue_circle_button_css');
 add_action('wp_enqueue_scripts', 'enqueue_circle_button_js');
