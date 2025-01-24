80c09b987dbf73b9ae2c81301a1f7dde4a58199a
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-01-24 16:45:12 -0700

    test

--- test-add-to-dom-plugin.php
+++ test-add-to-dom-plugin.php
@@ -39,9 +39,9 @@ function add_circle_buttons()
         <div class="circle-buttons">
             <h3>Choose a color:</h3>
             <div class="circle-buttons-container">
-                <button class="circle-button green-button" id="green-border-button"> </button>
-                <button class="circle-button red-button" id="red-border-button"> </button>
-                <button class="circle-button blue-button" id="blue-border-button"> </button>
+                <button class="circle-buttonp green-button" id="green-border-button"> </button>
+                <button class="circle-buttonp red-button" id="red-border-button"> </button>
+                <button class="circle-buttonp blue-button" id="blue-border-button"> </button>
             </div>
             <h3>Choose a texture:</h3>
         </div>
