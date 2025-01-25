50e82e38a19538ef68dfae0cf5b259b1927db307
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-01-24 23:13:33 -0700

    test

--- test-add-to-dom-plugin.php
+++ test-add-to-dom-plugin.php
@@ -23,26 +23,21 @@ function add_buttons()
     global $product;
 
     if (is_product()) {
-        
         ?>
-        <div class="added-content">
-            <div class="buttons">
-                <h3>Choose a color:</h3>
-                <div class="circle-buttons-container">
-                    <button class="circle-button green-button" id="green-border-button"></button>
-                    <button class="circle-button red-button" id="red-border-button"></button>
-                    <button class="circle-button blue-button" id="blue-border-button"></button>
-                </div>
-                <h3>Choose a texture:</h3>
-                <div class="circle-buttons-container">
-                    <button class="circle-button wood-button" id="wood-border-button"></button>
-                    <button class="circle-button metal-button" id="metal-border-button"></button>
-                    <button class="circle-button plastic-button" id="plastic-border-button"></button>
-                </div>
-                <br />
-            </div>            
-        <?php
-
+        <div class="buttons">
+            <h3>Choose a color:</h3>
+            <div class="circle-buttons-container">
+                <button class="circle-button green-button" id="green-border-button"></button>
+                <button class="circle-button red-button" id="red-border-button"></button>
+                <button class="circle-button blue-button" id="blue-border-button"></button>
+            </div>
+            <h3>Choose a texture:</h3>
+            <div class="circle-buttons-container">
+                <button class="circle-button wood-button" id="wood-border-button"></button>
+                <button class="circle-button metal-button" id="metal-border-button"></button>
+                <button class="circle-button plastic-button" id="plastic-border-button"></button>
+            </div>
+            <?php
     }
 }
 
@@ -61,12 +56,12 @@ function enqueue_model_viewer_script()
 function add_3d_model_viewer()
 {
     ?>
-    <div id="model-viewer-container">
-        <h1>3D Model Viewer</h1>
-        <model-viewer id="model-viewer" src="https://modelviewer.dev/shared-assets/models/Astronaut.glb"
-            alt="A 3D model of an astronaut" auto-rotate camera-controls ar></model-viewer>
-    </div>
-    <?php
+        <div id="model-viewer-container">
+            <h1>3D Model Viewer</h1>
+            <model-viewer id="model-viewer" src="https://modelviewer.dev/shared-assets/models/Astronaut.glb"
+                alt="A 3D model of an astronaut" auto-rotate camera-controls ar></model-viewer>
+        </div>
+        <?php
 }
 
 function test_add_to_dom_plugin()
