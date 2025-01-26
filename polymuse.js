5009697e0727338725a0fa7a6e817c3f1d6d1d05
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-01-25 17:23:18 -0700

    revert

--- polymuse.js
+++ polymuse.js
@@ -2,17 +2,14 @@ jQuery(document).ready(function($) {
     // Ensure the 3D model slide is the same height as other slides
     function adjustModelViewerHeight() {
         var galleryHeight = $('.woocommerce-product-gallery__wrapper').height();
+        //console.log(galleryHeight);
         $('.polymuse-model-viewer').height(500);
     }
 
     // Run on page load and when the window is resized
     adjustModelViewerHeight();
+    $(window).resize(adjustModelViewerHeight);
 
-    // Use passive event listener for resize to avoid blocking scroll
-    if (window.matchMedia('(pointer: fine)').matches) {
-        // This is a workaround if you're dealing with other event listeners being active
-        $(window).on('resize', { passive: true }, adjustModelViewerHeight);
-    } else {
-        $(window).resize(adjustModelViewerHeight);
-    }
 });
+
+
