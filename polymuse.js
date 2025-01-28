a642d2e60ae98523965d6b3ef142c62be5f2bf03
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-01-28 15:37:53 -0700

    added debugging to java script

--- polymuse.js
+++ polymuse.js
@@ -1,17 +1,20 @@
 jQuery(document).ready(function ($) {
     // Update hidden input field when a color button is clicked
     $('.circle-button').on('click', function () {
-      var color = $(this).attr('id');
-      $('#product_color_variation').val(color);
-    });
-  
+        console.log('Button clicked!');
+        var color = $(this).attr('data-color'); // Use data-color instead of id
+        console.log('Color:', color);
+        $('#product_color_variation').val(color);
+        console.log('Hidden input field value:', $('#product_color_variation').val());
+    })
+
     // Ensure the 3D model slide is the same height as other slides
     function adjustModelViewerHeight() {
-      var galleryHeight = $('.woocommerce-product-gallery__wrapper').height();
-      $('.polymuse-model-viewer').height(500);
+        var galleryHeight = $('.woocommerce-product-gallery__wrapper').height();
+        $('.polymuse-model-viewer').height(500);
     }
-  
+
     // Run on page load and when the window is resized
     adjustModelViewerHeight();
     $(window).resize(adjustModelViewerHeight);
-  });
+});
