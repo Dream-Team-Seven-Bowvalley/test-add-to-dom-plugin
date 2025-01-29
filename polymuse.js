0b1b975c0c11d81055250f39ac9aced191086162
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-01-28 23:50:19 -0700

    test

--- polymuse.js
+++ polymuse.js
@@ -2,9 +2,16 @@ jQuery(document).ready(function ($) {
     // Update hidden input field when a color button is clicked
     $('.circle-button').on('click', function () {
         var color = $(this).attr('data-color'); // Use data-color instead of id
-        $('#product_color_variation').val(color); 
-        $('#color').val(color);
+        var capitalColor = capitalizeFirstLetter(color); // Capitalize the first letter
+        $('#product_color_variation').val(color);
+        $('#color').val(capitalColor);
     });
+
+    // Function to capitalize the first letter of a string
+    function capitalizeFirstLetter(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
 
     // Ensure the 3D model slide is the same height as other slides
     function adjustModelViewerHeight() {
