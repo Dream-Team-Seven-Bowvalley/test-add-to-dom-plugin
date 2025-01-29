6bfa92a4044a5175815718d40d94d8ff9084778e
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-01-28 17:44:52 -0700

    turned on one more function

--- test-add-to-dom-plugin.php
+++ test-add-to-dom-plugin.php
@@ -188,7 +188,7 @@ function test_add_to_dom_plugin()
         add_action('woocommerce_before_add_to_cart_button', 'add_hidden_input_field_color_variation');
         add_filter('woocommerce_add_cart_item_data', 'update_product_variation', 10, 2);
         // add_filter('woocommerce_cart_item_name', 'display_variation_data', 10, 3);
-        // add_action('woocommerce_add_order_item_meta', 'add_variation_data_to_order_item_meta', 10, 3);
+        add_action('woocommerce_add_order_item_meta', 'add_variation_data_to_order_item_meta', 10, 3);
         // add_filter('woocommerce_email_order_item_meta', 'display_variation_data_in_email', 10, 2);
 
     }
