b2c35ab6d9bedb597b716609420217cbf2cc6db0
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-01-28 23:51:38 -0700

    test

--- polymuse.js
+++ polymuse.js
@@ -4,13 +4,18 @@ jQuery(document).ready(function ($) {
         var color = $(this).attr('data-color'); // Use data-color instead of id
         var capitalColor = capitalizeFirstLetter(color); // Capitalize the first letter
         $('#product_color_variation').val(color);
-        $('#color').val(capitalColor);
+        $('select[name="attribute_color"] option').each(function () {
+            if ($(this).val() === capitalColor) {
+                $(this).prop('selected', true);
+            }
+        });
     });
 
     // Function to capitalize the first letter of a string
     function capitalizeFirstLetter(str) {
         return str.charAt(0).toUpperCase() + str.slice(1);
     }
+
 
 
     // Ensure the 3D model slide is the same height as other slides
