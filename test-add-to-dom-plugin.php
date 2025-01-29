a9d5ace08bb0bc0ecf1b398343077a33a8a2aea0
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-01-28 20:40:09 -0700

    revert

--- test-add-to-dom-plugin.php
+++ test-add-to-dom-plugin.php
@@ -216,7 +216,7 @@ function test_add_to_dom_plugin()
 
         add_action('woocommerce_before_add_to_cart_button', 'add_hidden_input_field_color_variation');
         add_filter('woocommerce_add_cart_item_data', 'update_product_variation', 10, 2);
-        add_filter('woocommerce_email_order_items_args', 'add_color_variation_to_email', 10, 2);
+        // add_filter('woocommerce_email_order_items_args', 'add_color_variation_to_email', 10, 2);
     }
 }
 
