afa5e85ae47bf284a7cbcbbb4dfa522bfa4bd39e
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-01-29 00:10:18 -0700

    test

--- polymuse.js
+++ polymuse.js
@@ -1,16 +1,20 @@
+// jQuery(document).ready(function ($) {
+
+//     // Update hidden input field when a color button is clicked
+//     $('.circle-button').on('click', function () {
+//         console.log('Circle button clicked');
+//     });
+
+//     // Ensure the 3D model slide is the same height as other slides
+//     function adjustModelViewerHeight() {
+//         var galleryHeight = $('.woocommerce-product-gallery__wrapper').height();
+//         $('.polymuse-model-viewer').height(500);
+//     }
+//     // Run on page load and when the window is resized
+//     adjustModelViewerHeight();
+//     $(window).resize(adjustModelViewerHeight);
+// });
+
 jQuery(document).ready(function ($) {
-    // Update hidden input field when a color button is clicked
-    $('.circle-button').on('click', function () {
-        console.log('Circle button clicked');
-    });
-
-    // Ensure the 3D model slide is the same height as other slides
-    function adjustModelViewerHeight() {
-        var galleryHeight = $('.woocommerce-product-gallery__wrapper').height();
-        $('.polymuse-model-viewer').height(500);
-    }
-    // Run on page load and when the window is resized
-    adjustModelViewerHeight();
-    $(window).resize(adjustModelViewerHeight);
+    console.log("Polymuse plugin JS loaded successfully!");
 });
-
