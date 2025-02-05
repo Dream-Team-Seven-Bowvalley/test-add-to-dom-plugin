18499abadc54aa8afcf89f513eca8671dda450d4
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-02-05 15:36:52 -0700

    test new js

--- polymuse.js
+++ polymuse.js
@@ -1,42 +1,44 @@
 jQuery(document).ready(function ($) {
-
     function adjustModelViewerHeight() {
-        $('.polymuse-model-viewer').height(500);
+      $('.polymuse-model-viewer').height(500);
     }
-
+  
     adjustModelViewerHeight();
-    $(window).resize(adjustModelViewerHeight);
-
-    // Check if the model viewer exists
+    $(window).resize({ passive: true }, adjustModelViewerHeight);
+  
     const modelViewer = $('.polymuse-model-viewer')[0];
-
-    // If model viewer exists, listen for the load event
+  
     if (modelViewer) {
-
-        // Wait for the model to be fully loaded
-        const model = modelViewer.model;
-        const materials = modelViewer.model.materials;
-        const variants = modelViewer.availableVariants || [];  // Adjust this logic if needed
-
-        console.log('model', model);
-        console.log('materials', materials);
-        console.log('variants', variants);
-
-        // Add buttons for variants after model is loaded
-        const variantButtonsContainer = $('#variant-options-container');
-        // variantButtonsContainer.empty();  // Clear previous buttons
-
-        if (variants.length > 0) {
+      const checkModelLoaded = () => {
+        if (modelViewer.model && modelViewer.model.materials) {
+          const model = modelViewer.model;
+          const materials = modelViewer.model.materials;
+          const variants = modelViewer.availableVariants || [];
+  
+          console.log('model', model);
+          console.log('materials', materials);
+          console.log('variants', variants);
+  
+          // Add buttons for variants after model is loaded
+          const variantButtonsContainer = $('#variant-options-container');
+          variantButtonsContainer.empty();
+  
+          if (variants.length > 0) {
             variants.forEach(variant => {
-                const button = $('<button></button>');
-                button.text(variant); // Set button text as the variant name
-                button.on('click', function () {
-                    modelViewer.variantName = variant;  // Update model viewer with the selected variant
-                });
-                variantButtonsContainer.append(button);
+              const button = $('<button></button>');
+              button.text(variant);
+              button.on('click', function () {
+                modelViewer.variantName = variant;
+              });
+              variantButtonsContainer.append(button);
             });
-        } else {
+          } else {
             variantButtonsContainer.text('No variants available');
+          }
+        } else {
+          setTimeout(checkModelLoaded, 100);
         }
+      };
+      checkModelLoaded();
     }
-});
+  });
