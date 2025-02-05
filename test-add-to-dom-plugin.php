3cc7dd04176d7f4844b6e458d0cf22befd2ba206
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-02-05 15:18:59 -0700

    cleaned node files

--- test-add-to-dom-plugin.php
+++ test-add-to-dom-plugin.php
@@ -15,28 +15,6 @@
  * Requires Plugins:  WooCommerce
  */
 
-
-// Add Placeholder image to product to hide later to make selection stay on the model
-// Set Product image as placeholder using local image
-function set_default_placeholder_product_image_from_url($post_id)
-{
-    if (get_post_type($post_id) !== 'product') {
-        return;
-    }
-
-    // URL to the placeholder image
-    $placeholder_image_url = get_site_url() . '/wp-content/uploads/woocommerce-placeholder.png';
-
-    // Get the attachment ID from the URL
-    $attachment_id = attachment_url_to_postid($placeholder_image_url);
-
-    // Check if the product already has an image
-    if (!has_post_thumbnail($post_id) && $attachment_id) {
-        // Set the placeholder image as the product thumbnail
-        set_post_thumbnail($post_id, $attachment_id);
-    }
-}
-
 // Add custom field to product editor
 function polymuse_custom_field_model_url()
 {
@@ -50,35 +28,12 @@ function polymuse_custom_field_model_url()
     );
 }
 
-function polymuse_custom_field_variant_json_data()
-{
-    woocommerce_wp_text_input(
-        array(
-            'id' => '_variant_json_data',
-            'label' => 'Variant JSON Data',
-            'description' => 'Enter the JSON data for the variant product',
-            'desc_tip' => true,
-        )
-    );
-}
-
 // Save custom field data
 function polymuse_save_custom_field_model_url($post_id)
 {
     $model_url = $_POST['_3d_model_url'];
     if (!empty($model_url)) {
         update_post_meta($post_id, '_3d_model_url', esc_url($model_url));
-    }
-}
-
-function polymuse_save_custom_field_variant_json_data($post_id)
-{
-    $variant_json_data = $_POST['_variant_json_data'];
-    if (!empty($variant_json_data)) {
-        // Sanitize the JSON data
-        $sanitized_json_data = wp_kses_post($variant_json_data);
-        // Save the sanitized JSON data
-        update_post_meta($post_id, '_variant_json_data', $sanitized_json_data);
     }
 }
 
@@ -128,49 +83,9 @@ function polymuse_add_model_and_thumbnail_to_gallery($html, $attachment_id)
 
     return $html;
 }
-// Add Buttons to product page stable
-function add_buttons()
-{
-    global $product;
 
-    if (is_product()) {
-        // Retrieve the JSON data from the product meta
-        $variant_json_data = get_post_meta($product->get_id(), '_variant_json_data', true);
 
-        // Decode the JSON data
-        $json_data = json_decode($variant_json_data, true);
-
-        ?>
-        <div>
-            <?php foreach ($json_data as $variant_group) { ?>
-                <h3><?php echo esc_html($variant_group['title']); ?>:</h3>
-                <div>
-                    <?php foreach ($variant_group['variants'] as $variant) {
-                        $variant_title = esc_attr($variant['title']);
-                        $variant_value = isset($variant['value']) ? esc_attr($variant['value']) : null;
-                        ?>
-                        <!-- Maybe add a check for hex value here as well -->
-                        <?php if ($variant_value) { ?>
-                            <!-- If color hex exists, use it for background -->
-                            <button class="circle-button" id="<?php echo $variant_title; ?>-button"
-                                data-color="<?php echo $variant_value; ?>" style="background-color: <?php echo $variant_value; ?>">
-                            </button>
-                        <?php } else { ?>
-                            <!-- If no hex, use regular button -->
-                            <button class="wp-element-button" id="<?php echo $variant_title; ?>-button">
-                                <?php echo $variant_title; ?>
-                            </button>
-                        <?php } ?>
-                    <?php } ?>
-                </div>
-            <?php } ?>
-            <br />
-        </div>
-        <?php
-    }
-}
-
-// Attempt to change model info
+// Add variant style buttons container to product page
 function add_buttons2()
 {
     global $product;
@@ -203,8 +118,7 @@ function test_add_to_dom_plugin()
         in_array($plugin_path, wp_get_active_and_valid_plugins())
         || in_array($plugin_path, wp_get_active_network_plugins())
     ) {
-        // The plugin works correctly when there is a default that is hidden(allowing the 3d model to takes its place)
-        add_action('save_post', 'set_default_placeholder_product_image_from_url', 10, 1);
+       
 
         // Add 3D model URL and Json data field to product editor
         add_action('woocommerce_product_options_general_product_data', 'polymuse_custom_field_model_url');
