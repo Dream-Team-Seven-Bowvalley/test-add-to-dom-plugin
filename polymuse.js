47b8d428ec64254e343931640c7fbcfba8cb0573
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-02-05 16:59:45 -0700

    test

--- polymuse.js
+++ polymuse.js
@@ -9,31 +9,13 @@ jQuery(document).ready(function ($) {
 
     const modelViewer = $('.polymuse-model-viewer')[0];
 
-    if (modelViewer) {
-        console.log('Model viewer found');
 
-        if (modelViewer.loaded) {
-            console.log('Model viewer already loaded');
-        } else {
-            console.log('Model viewer not loaded');
-            setTimeout(() => {
-                if (modelViewer.loaded) {
-                    console.log('Model viewer loaded');
-                }
-                else {
-                    console.log('Model viewer not loaded');
-                }
-            }, 3000);
-        }
 
-        modelViewer.addEventListener('loaded', () => {  // The ONLY reliable way
-            console.log('Model viewer loaded (event fired)');
-            // Your code to work with the model goes HERE.
-            // Now you can access modelViewer.model, etc.
-        });
+    modelViewer.addEventListener('load', () => {  
+        console.log('Model viewer loaded (event fired)');
+    });
 
-        modelViewer.addEventListener('error', (error) => {
-            console.error('Model viewer loading error:', error);
-        });
-    }
+    modelViewer.addEventListener('error', (error) => {
+        console.error('Model viewer loading error:', error);
+    });
 });
