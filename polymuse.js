e508899eb7e881232f11a9a91763d7e6edde42ef
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-02-05 17:06:29 -0700

    added js

--- polymuse.js
+++ polymuse.js
@@ -7,6 +7,30 @@ jQuery(document).ready(function ($) {
     adjustModelViewerHeight();
     $(window).resize(adjustModelViewerHeight);
 
+
+    function checkModelViewerLoaded() {
+        if (window.ModelViewer) {
+            console.log('Model Viewer library loaded!');
+            const modelViewer = $('.polymuse-model-viewer')[0]; // Use jQuery selector
+            if (modelViewer) {
+                modelViewer.addEventListener('load', () => {
+                    console.log('Model viewer loaded (event fired)');
+                    // Your code to work with the model goes here
+                });
+                modelViewer.addEventListener('error', (error) => {
+                    console.error('Model viewer loading error:', error);
+                });
+            } else {
+                console.log('Model Viewer element not found.');
+            }
+        } else {
+            console.log('Model Viewer library not yet loaded. Checking again...');
+            setTimeout(checkModelViewerLoaded, 100);
+        }
+    }
+
+    checkModelViewerLoaded();
+
     const modelViewer = $('.polymuse-model-viewer')[0];
 
     if (modelViewer) {
